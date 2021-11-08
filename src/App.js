e9b7ac41216801e132863613e289d1c4a73dfe77
alfathmuqoddas <alfath.muqoddas@gmail.com>
2021-11-08 20:19:03 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { html } from 'https://unpkg.com/htm/preact/standalone.module.js';
+
+export const Za = () => {
+	return html`<h1>This is app.js</h1>`;
+}
+
+export const Foo = () => {
+	return html`<h1>This is foo</h1>`
+}
