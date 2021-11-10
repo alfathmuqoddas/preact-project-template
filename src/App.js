b9a3f80f20cd2d63c5ddc5ceebed4473cb2ceae9
alfathmuqoddas <alfath.muqoddas@gmail.com>
2021-11-10 23:18:49 +0700

    deleting folder

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { html } from 'https://unpkg.com/htm/preact/standalone.module.js';
-
-export const Za = () => {
-	return html`<h1>This is app.js</h1>`;
-}
-
-export const Foo = () => {
-	return html`<h1>This is foo</h1>`
-}
